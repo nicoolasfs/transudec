7f0499c997bec7fe13521a600198cde1f36ef4b1
LuchoGuel07 <lu_gel07@hotmail.com>
2021-09-20 10:41:51 -0500

    Se modifican tablas y se agrega correctamente orden a la tabla

--- src/app/pages/conductores/conductores.component.ts
+++ src/app/pages/conductores/conductores.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgProgress } from 'ngx-progressbar';
+import { Departamento } from 'src/app/_model/Departamento';
+import { DepartamentoService } from 'src/app/_service/departamento.service';
 import { ProgressBarService } from 'src/app/_service/progress-bar.service';
 
 @Component({
@@ -8,14 +10,20 @@ import { ProgressBarService } from 'src/app/_service/progress-bar.service';
   styleUrls: ['./conductores.component.css']
 })
 export class ConductoresComponent implements OnInit {
+  departamentos: Departamento;
 
-  constructor(private progress: NgProgress, public progressBar: ProgressBarService) { }
+  constructor(private departamentoService: DepartamentoService,private progress: NgProgress, public progressBar: ProgressBarService) { }
 
   ngOnInit(): void {
     this.progressBar.progressRef = this.progress.ref('progressBar');
+    console.log("Antes de llamar al servicio");
     this.progressBar.startLoading();
+    this.departamentoService.listar().subscribe(data =>{
+      console.log(data);
+      data.forEach(element =>{
 
-    this.progressBar.completeLoading();
-  }
-
-}
+      console.log(`Código: ${element.idDepartamento} - Nombre: ${element.nombre}`);
+      this.progressBar.completeLoading();
+    });
+  });
+}}
--- src/app/pages/editar/editar.component.ts
+++ src/app/pages/editar/editar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgProgress } from 'ngx-progressbar';
+import { Departamento } from 'src/app/_model/Departamento';
+import { DepartamentoService } from 'src/app/_service/departamento.service';
 import { ProgressBarService } from 'src/app/_service/progress-bar.service';
 
 @Component({
@@ -7,13 +9,26 @@ import { ProgressBarService } from 'src/app/_service/progress-bar.service';
   templateUrl: './editar.component.html',
   styleUrls: ['./editar.component.css']
 })
-export class EditarComponent implements OnInit {
 
-  constructor(private progress: NgProgress, public progressBar: ProgressBarService) { }
+export class EditarComponent implements OnInit {
+  departamentos: Departamento;
+  constructor(private departamentoService: DepartamentoService,private progress: NgProgress, public progressBar: ProgressBarService) { }
 
   ngOnInit(): void {
+    this.progressBar.progressRef = this.progress.ref('progressBar');
+    console.log("Antes de llamar al servicio");
     this.progressBar.startLoading();
-    this.progressBar.completeLoading();
-  }
+    this.departamentoService.listar().subscribe(data =>{
+      console.log(data);
+      data.forEach(element =>{
 
-}
+      console.log(`Código: ${element.idDepartamento} - Nombre: ${element.nombre}`);
+      this.progressBar.completeLoading();
+
+
+    });
+  });
+}}
+
+
+
